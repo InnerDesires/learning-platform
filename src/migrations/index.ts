44d112462b995c516704a23c0ee5862a78e167d5
InnerDesires <vlad.tertyshny@gmail.com>
2026-03-01 22:09:54 +0200

    fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20260225_105915 from './20260225_105915';
 import * as migration_20260226_140344 from './20260226_140344';
 import * as migration_20260227_181154_better_auth from './20260227_181154_better_auth';
+import * as migration_20260301_200000_mcp_plugin from './20260301_200000_mcp_plugin';
 
 export const migrations = [
   {
@@ -17,5 +18,10 @@ export const migrations = [
     up: migration_20260227_181154_better_auth.up,
     down: migration_20260227_181154_better_auth.down,
     name: '20260227_181154_better_auth',
+  },
+  {
+    up: migration_20260301_200000_mcp_plugin.up,
+    down: migration_20260301_200000_mcp_plugin.down,
+    name: '20260301_200000_mcp_plugin',
   },
 ];
